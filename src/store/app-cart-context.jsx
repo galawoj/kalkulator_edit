6dde7aa7d321bcdf2c735224c2111f34590eaafb
unknown <galawoj@gmail.com>
2024-02-04 22:04:45 +0100

    registration loader correction

--- src/store/app-cart-context.jsx
+++ src/store/app-cart-context.jsx
@@ -128,6 +128,7 @@ export default function ContextProvider ({children}) {
             })
             .catch((error) => {
               setRegistrationSucces(error)
+              setWaitingForRegistration(false)
             });
       
       
